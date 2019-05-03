e2bfea96161ed74c6097b45530795cb3a61a2cab
Kalobi <46748261+Kalobi@users.noreply.github.com>
2019-05-03 11:59:47 +0200

    cleanup 3

---
+++ Prog2/de.uni-hannover.hci.mario_luensmann/src/datareader/Data.java
@@ -0,0 +1,16 @@
+package datareader;
+
+public class Data {
+	
+	public double[][] myTrainData;
+	
+	public int[] myTrainOutput;
+	
+	//private MainClass myMain1;
+	
+	public Data(double[][] pMyTrainData, int[] pMyTrainOut){
+		this.myTrainData = pMyTrainData;
+		this.myTrainOutput = pMyTrainOut;
+	}
+
+}
